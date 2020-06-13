605db06eece64ee49c93a43e8952e6362e644b65
Sam <samrolandwaters@gmail.com>
2020-06-12 22:59:22 -0700

    a lot!

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
+  activeTab = "about";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.setActiveTab(this.getActiveTab());
   }
 
+  ngOnInit() {}
+
+  getActiveTab() {
+    return new URL(window.location.href).hash.substring(2);
+  }
+
+  setActiveTab(activeTab: string) {
+    this.activeTab = activeTab;
+  }
 }
